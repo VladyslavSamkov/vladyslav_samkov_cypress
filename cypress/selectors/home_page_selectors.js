5880b8fea9c3ef7bbd37c750c06cd630e96f5e3b
Vladyslav <kr3w2013@gmail.com>
2022-08-25 18:37:48 +0300

    Fix home page

--- cypress/selectors/home_page_selectors.js
+++ cypress/selectors/home_page_selectors.js
@@ -1,6 +1,5 @@
 export const home_page_selectors = {
     logout_btn: '[data-test="sidenav-signout"]',
     ba_btn: '[data-test="sidenav-bankaccounts"]',
-    new_transaction_btn: '[data-test="nav-top-new-transaction"]',
-    user_devon_backer: '[data-test="data-test="user-list-item-tsHF6_D5oQ"]'
+    new_transaction_btn: '[data-test="nav-top-new-transaction"]'
 }
