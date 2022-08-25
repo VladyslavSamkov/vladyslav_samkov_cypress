affc5dc5cc743cfb480565ed3f0401fb945a1d71
Vlad Samkov <108725927+VladyslavSamkov@users.noreply.github.com>
2022-08-25 18:40:43 +0300

    Update home_page_selectors.js

--- cypress/selectors/home_page_selectors.js
+++ cypress/selectors/home_page_selectors.js
@@ -1,4 +1,5 @@
 export const home_page_selectors = {
     logout_btn: '[data-test="sidenav-signout"]',
     ba_btn: '[data-test="sidenav-bankaccounts"]',
+    new_transaction_btn: '[data-test="nav-top-new-transaction"]'
 }
